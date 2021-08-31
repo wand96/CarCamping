a518dd162ba780cc326f15cfac3b4fe0f1a06d0b
wand96 <tjsrb96@naver.com>
2021-08-31 21:21:43 +0900

    [feature] 초기작업

---
+++ app/src/main/java/com/example/carcamping/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.carcamping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.carcamping.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = listOf(Fragment_map(), Fragment_BookMark(), Fragment_Snap())
+
+        val pagerAdapter = FragmentPagerAdapter(list, this)
+
+        binding.viewPager.adapter = pagerAdapter
+
+        val titles = listOf("Map", "BookMark", "Snap")
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, position ->
+            tab.text = titles.get(position)
+        }.attach()
+    }
+}
+
+class FragmentPagerAdapter(val fragmentList:List<Fragment>, fragmentActivity: FragmentActivity):
+                                                        FragmentStateAdapter(fragmentActivity){
+    override fun getItemCount() = fragmentList.size
+    override fun createFragment(position: Int) = fragmentList.get(position)
+
+}
