32be177152db54f60a374b96642feff2d2b6cbaf
Parkduksung <duksung1234@naver.com>
2021-08-31 22:42:50 +0900

    [feature-package] ui package 구성.

--- app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
@@ -1,8 +1,9 @@
-package com.example.carcamping
+package com.example.carcamping.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.carcamping.ui.home.HomeActivity
 
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
