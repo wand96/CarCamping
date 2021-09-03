de9b0685d73926ccce9e15744b64e59b7971ee8b
wand96 <tjsrb96@naver.com>
2021-09-03 15:17:19 +0900

    [feature] 스플래쉬화면 1.5초 뒤에 넘어가는 코드(error)

--- app/src/main/java/com/example/carcamping/SplashActivity.kt
+++ app/src/main/java/com/example/carcamping/SplashActivity.kt
@@ -2,15 +2,24 @@ package com.example.carcamping
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-        val intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
 
+    override fun onPause() {
+        super.onPause()
         finish()
     }
 }
