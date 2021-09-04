556544df1c8be316d6a9a20babdd2842bae68552
Parkduksung <duksung1234@naver.com>
2021-09-04 10:37:18 +0900

    [feature] SplashActivity 화면 전환 관련 리팩토링.

--- app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.carcamping.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carcamping.R
 import com.example.carcamping.ui.HomeActivity
@@ -12,13 +13,27 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        var handler = Handler()
-        handler.postDelayed({
-            var intent = Intent(this, HomeActivity::class.java).addFlags(
-                Intent.FLAG_ACTIVITY_CLEAR_TASK
-            )
-            startActivity(intent)
-        }, 1500)
+        routeHomeActivity()
     }
 
+
+
+    private fun routeHomeActivity() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            Runnable {
+                val intent = Intent(this@SplashActivity, HomeActivity::class.java).addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+                )
+                startActivity(intent)
+            },
+            DELAY_ROUTE_TIME
+        )
+    }
+
+
+    companion object {
+        private const val DELAY_ROUTE_TIME = 1500L
+
+    }
 }
+
