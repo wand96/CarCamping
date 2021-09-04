d68351a5d5d3b41eef0eb753cf2a9c97d348039f
Parkduksung <duksung1234@naver.com>
2021-09-04 10:57:30 +0900

    [feature] package home 으로 이동.

--- app/src/main/java/com/example/carcamping/ui/home/MapFragment.kt
+++ app/src/main/java/com/example/carcamping/ui/home/MapFragment.kt
@@ -1,4 +1,4 @@
-package com.example.carcamping.ui
+package com.example.carcamping.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.carcamping.R
 
-class BookMarkFragment : Fragment() {
+class MapFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.bookmark_fragment, container, false)
+        return inflater.inflate(R.layout.map_fragment, container, false)
     }
 
 }
--- app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
@@ -6,7 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carcamping.R
-import com.example.carcamping.ui.HomeActivity
+import com.example.carcamping.ui.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
