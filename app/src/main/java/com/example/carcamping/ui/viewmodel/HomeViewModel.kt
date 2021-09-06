d6aaa8390086079e7a9b2c3d6ae08455399232bc
Parkduksung <dspark@rsupport.com>
2021-09-06 16:40:28 +0900

    [feature] HomeViewModel 필요한 함수나 Sealed class 등 생성.

--- app/src/main/java/com/example/carcamping/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/carcamping/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,26 @@
 package com.example.carcamping.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.carcamping.ui.data.model.GoCampingItem
 
 class HomeViewModel : ViewModel() {
+
+    private val _homeViewStateLiveData = MutableLiveData<HomeViewState>()
+    val homeViewStateLiveData : LiveData<HomeViewState> = _homeViewStateLiveData
+
+
+    fun getGoCampingBasedList() {
+
+
+    }
+
+
+
+    sealed class HomeViewState {
+        data class GetGoCampingBasedList(val goCampingItem: GoCampingItem) : HomeViewState()
+        object ErrorGetGoCampingBasedList : HomeViewState()
+    }
+
 }
