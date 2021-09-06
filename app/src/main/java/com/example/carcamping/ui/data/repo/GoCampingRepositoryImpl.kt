e985698302f4c37609c9d0c00a16f4e8c0d370b6
Parkduksung <duksung1234@naver.com>
2021-09-06 16:33:23 +0900

    [feature] 고캠핑 BasedList 가져오는 Repository 부분 작업.

--- app/src/main/java/com/example/carcamping/ui/data/repo/GoCampingRepositoryImpl.kt
+++ app/src/main/java/com/example/carcamping/ui/data/repo/GoCampingRepositoryImpl.kt
@@ -1,4 +1,15 @@
 package com.example.carcamping.ui.data.repo
 
-class GoCampingRepositoryImpl : GoCampingRepository {
+import com.example.carcamping.ui.api.response.GoCampingResponse
+import com.example.carcamping.ui.data.source.remote.GoCampingRemoteDataSource
+
+class GoCampingRepositoryImpl(private val goCampingRemoteDataSource: GoCampingRemoteDataSource) :
+    GoCampingRepository {
+
+    override fun getBasedList(
+        onSuccess: (goCampingResponse: GoCampingResponse) -> Unit,
+        onFailure: (throwable: Throwable) -> Unit
+    ) {
+        goCampingRemoteDataSource.getBasedList(onSuccess, onFailure)
+    }
 }
