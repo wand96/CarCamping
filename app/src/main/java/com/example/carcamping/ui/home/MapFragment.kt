cbc4c41c160e3e03920803466b615bd49446d8ad
Parkduksung <duksung1234@naver.com>
2021-09-12 18:40:30 +0900

    [feature] kakaoMap 추가.

--- app/src/main/java/com/example/carcamping/ui/home/MapFragment.kt
+++ app/src/main/java/com/example/carcamping/ui/home/MapFragment.kt
@@ -1,19 +1,70 @@
 package com.example.carcamping.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.carcamping.R
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.carcamping.databinding.MapFragmentBinding
+import net.daum.mf.map.api.MapView
 
 class MapFragment : Fragment() {
+
+    private lateinit var binding: MapFragmentBinding
+
+    private lateinit var mapView: MapView
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.map_fragment, container, false)
+    ): View {
+        binding = MapFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun loadMapView() {
+        mapView = MapView(context)
+        binding.containerMap.addView(mapView)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            999 -> {
+                loadMapView()
+            }
+
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 999)
+        } else {
+            loadMapView()
+        }
+
     }
 
 }
