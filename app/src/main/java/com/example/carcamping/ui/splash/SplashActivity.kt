ca30cbd6bfd8fc78bb9e388ae379ace2d0b3ea79
wand96 <tjsrb96@naver.com>
2021-09-12 16:02:42 +0900

    [feature] kakao map 세팅

--- app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/carcamping/ui/splash/SplashActivity.kt
@@ -1,12 +1,19 @@
 package com.example.carcamping.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Base64
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carcamping.R
 import com.example.carcamping.ui.home.HomeActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +21,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         routeHomeActivity()
+        getHashKey()
     }
 
 
@@ -30,6 +38,26 @@ class SplashActivity : AppCompatActivity() {
         )
     }
 
+    @SuppressLint("PackageManagerGetSignatures")
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
+    }
 
     companion object {
         private const val DELAY_ROUTE_TIME = 1500L
